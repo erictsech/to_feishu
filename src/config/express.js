85b6860028feed19fe01db90fab51468079df430
erictsech <73739759+erictsech@users.noreply.github.com>
2024-04-19 13:53:42 +0800

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -5,11 +5,12 @@ const compress = require('compression');
 const methodOverride = require('method-override');
 const cors = require('cors');
 const helmet = require('helmet');
-const passport = require('passport');
-const routes = require('../api/routes/v1');
+//const passport = require('passport');
+//const routes = require('../api/routes/v1');
 const { logs } = require('./vars');
-const strategies = require('./passport');
+//const strategies = require('./passport');
 const error = require('../api/middlewares/error');
+//const {mysqltest} = require('../mysql.js');
 
 /**
 * Express instance
@@ -22,29 +23,14 @@ app.use(morgan(logs));
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use("/sqlconn",mysqltest);
+app.get("/a2p", (req, res) => {
+    //const {username,password} = req.body
+    res.send(JSON.stringify("abc"))
+    console.log(req.body)
+  });
 
-// gzip compression
-app.use(compress());
-
-// lets you use HTTP verbs such as PUT or DELETE
-// in places where the client doesn't support it
-app.use(methodOverride());
-
-// secure apps by setting various HTTP headers
-app.use(helmet());
-
-// enable CORS - Cross Origin Resource Sharing
-app.use(cors());
-
-// enable authentication
-app.use(passport.initialize());
-passport.use('jwt', strategies.jwt);
-passport.use('facebook', strategies.facebook);
-passport.use('google', strategies.google);
-
-// mount api v1 routes
-app.use('/v1', routes);
 
 // if error is not an instanceOf APIError, convert it.
 app.use(error.converter);
